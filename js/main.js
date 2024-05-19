ee9ef6f6891e12e92dff8e3e347f6da5d8a5f7f3
ALEKSANDR FIZIK <109596308+komsander@users.noreply.github.com>
2024-05-19 21:46:57 +1000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+  $(".bike-slider,.slider__items").slick({
+    arrows: false,
+    dots: true,
+    // стиль переключения
+    fade: true,
+    // автопереключение
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+// $(function () {
+//   $(".slider__items").slick({
+//     arrows: false,
+//     dots: true,
+//     // стиль переключения
+//     fade: true,
+//     // автопереключение
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+// });
